82df60d9df03672e037a369dd542ab32d23a04dd
napi <modcoot@gmail.com>
2022-06-16 22:57:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+  function openPage(id) {
+    //Hide All Tabs
+    document.getElementById('home').style.display="none";
+    document.getElementById('contact').style.display="none";
+
+    //Show the Selected Tab
+    document.getElementById(id).style.display="block";  
+  }
