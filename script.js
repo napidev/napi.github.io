0166b9023f150afcb072fa638b96c0c731a216f3
napi <modcoot@gmail.com>
2022-06-17 00:13:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
     //Hide All Tabs
     document.getElementById('home').style.display="none";
     document.getElementById('contact').style.display="none";
+    document.getElementById('skills').style.display="none";
 
     //Show the Selected Tab
     document.getElementById(id).style.display="block";  
